8b858d0d2e9c4605fc87826c176fa5cb3c1bdb5c
Ginner Zapata <gzapata@vizzn.ca>
2023-01-11 23:36:53 -0300

    cords

--- models/server.js
+++ models/server.js
@@ -6,6 +6,8 @@ const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
 
+const cors = require("cors");
+
 class Server {
   constructor() {
     this.app = express();
@@ -22,6 +24,7 @@ class Server {
 
   middlewares() {
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+    this.app.use(cors());
   }
   configurarSockets() {
     new Sockets(this.io);
