849b165a1ed63fa22a98ee88b3f5bcb9498410a5
Ginner Zapata <gzapata@vizzn.ca>
2023-01-11 23:14:26 -0300

    feat: server to class

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-//servidor express
-const express = require("express");
-const app = express();
+const Server = require("./models/server");
+require("dotenv").config();
 
-//servidor sockets
-const server = require("http").createServer(app);
+const server = new Server();
 
-//configuracion del socket server
-const io = require("socket.io")(server);
-
-//desplegar el directorio publico
-app.use(express.static(__dirname + "/public"));
-
-io.on("connection", (socket) => {
-  socket.emit("mensaje-bienvenida", {
-    message: "Bienvenido al cinco letras oh oh",
-    fecha: new Date(),
-  });
-  socket.on("mensaje-to-server", (data) => {
-    console.log(data);
-    io.emit("mensaje-from-server", data);
-  });
-});
-
-server.listen(8080, () => {
-  console.log("Server corriendo en puerto: 8080");
-});
+server.execute();
